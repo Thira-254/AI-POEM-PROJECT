dd2debfed208da6af42d48cf19d39130fb21747e
Thira-254 <agathathira60@gmail.com>
2026-03-19 17:54:32 +0300

    added api integration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 function generatePoem(response) {
-  formElement.innerHTML = response.target.innerText;
+  response.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "Poem goes here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 
 let formElement = document.querySelector("form");
-formElement.addEventListener("click", generatePoem);
+formElement.addEventListener("submit", generatePoem);
